eee5e965558557e25db8c59937b53ac98ac5269b
Thejus Thomson <thomson.th@northeastern.edu>
2024-02-15 03:37:54 -0500

    [Assignment 3] - Adding dynamic variables

--- variables.tf
+++ variables.tf
@@ -40,6 +40,24 @@ variable "default_compute_route_ip" {
   default     = "0.0.0.0/0"
 }
 
+variable "app_subnetwork" {
+  description = "name of app subnetwork"
+  type        = string
+  default     = "webapp"
+}
+
+variable "db_subnetwork" {
+  description = "name of db subnetwork"
+  type        = string
+  default     = "db"
+}
+
+variable "default_compute_route" {
+  description = "name of db default compute route"
+  type        = string
+  default     = "default-compute-route"
+}
+
 variable "vpc_count" {
   description = "number of VPCs"
 }
